bf0216b9edf580b288da821b4b0ee14704dfd214
Frantz Kati <bahdcoder@gmail.com>
2020-07-25 23:27:43 +0100

    16-animate-select-caret

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -48,7 +48,7 @@ const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'P
         <button ref={labelRef} className='dse-select__label' onClick={() => onLabelClick()}>
             <Text>{selectedOption === null ? label : selectedOption.label}</Text>
 
-            <svg width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
+            <svg className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`} width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
         </button>
 
         {isOpen ? (
