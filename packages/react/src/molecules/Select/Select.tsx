5f060b1fb1e64731b577ffdc8bb84150e4a65a83
Frantz Kati <bahdcoder@gmail.com>
2020-07-25 22:55:35 +0100

    13-calculate-overlay-position

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React,  { useState } from 'react'
+import React,  { useState, useRef, useEffect } from 'react'
 
 interface SelectOption {
     label: string
@@ -13,6 +13,8 @@ interface SelectProps {
 
 const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'Please select an option ...', onOptionSelected: handler }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const labelRef = useRef<HTMLButtonElement>(null);
+    const [overlayTop, setOverlayTop] = useState<number>(0)
 
     const onOptionSelected = (option: SelectOption, optionIndex: number) => {
         if (handler) {
@@ -24,15 +26,21 @@ const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'P
         setIsOpen(!isOpen)
     }
 
+    useEffect(() => {
+        setOverlayTop((
+            labelRef.current?.offsetHeight || 0
+        ) + 10)
+    }, [labelRef.current?.offsetHeight])
+
     return <div className='dse-select'>
-        <button className='dse-select__label' onClick={() => onLabelClick()}>
+        <button ref={labelRef} className='dse-select__label' onClick={() => onLabelClick()}>
             <span>{label}</span>
 
             <svg width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
         </button>
 
         {isOpen ? (
-            <ul className='dse-select__overlay'>
+            <ul style={{ top: overlayTop }} className='dse-select__overlay'>
                 {options.map((option, optionIndex) => {
                     return <li onClick={() => onOptionSelected(option, optionIndex)} key={option.value}>{option.label}</li>
                 })}
